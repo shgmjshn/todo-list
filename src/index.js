05dc292a82ec457f6a32b206619dbaa41939cce2
shgmjshn <shogimajishan0312@gmail.com>
2024-10-11 11:31:15 +0900

    c:\Users\shogi\OneDrive\デスクトップ\リスト\todo-list\src\index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
-const el = document.getElementById("root");
-
-const root = ReactDOM.createRoot(el);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
 function App() {
   const [inputValue, setInputValue] = React.useState('');
   const [todos, setTodos] = React.useState([]);
+  const [filterMode, setFilterMode] = React.useState('all'); // 'all', 'incomplete', 'complete'
 
   const handleAddTodo = (e) => {
     e.preventDefault();
@@ -16,13 +16,38 @@ function App() {
       alert('Todoを入力してください');
       return;
     }
-    setTodos([...todos, inputValue]);
+    setTodos([...todos, { text: inputValue, done: false }]);
     setInputValue('');
   }
 
+  const handleDoneTodo = (index) => {
+    setTodos(todos.map((todo, i) => {
+      if (i === index) {
+        return { ...todo, done: true };
+      }
+      return todo;
+    }));
+  }
+  
   const handleDeleteTodo = (index) => {
     setTodos(todos.filter((_, i) => i !== index));
   }
+
+  const toggleFilterMode = () => {
+    if (filterMode === 'all') {
+      setFilterMode('incomplete');
+    } else if (filterMode === 'incomplete') {
+      setFilterMode('complete');
+    } else {
+      setFilterMode('all');
+    }
+  }
+
+  const filteredTodos = todos.filter(todo => {
+    if (filterMode === 'incomplete') return !todo.done;
+    if (filterMode === 'complete') return todo.done;
+    return true;
+  });
 
   return (
     <div>
@@ -37,10 +62,17 @@ function App() {
         />
         <button type="submit">追加</button>
       </form>
+      <button onClick={toggleFilterMode}>
+        {filterMode === 'all' && '未完了のTodoのみ表示'}
+        {filterMode === 'incomplete' && '完了のTodoのみ表示'}
+      </button>
       <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>
-            {todo}
+        {filteredTodos.map((todo, index) => (
+          <li key={index} style={{ color: todo.done ? 'red' : 'black' }}>
+            {todo.text}{todo.done ? '(完了)' : ''}
+            {!todo.done && (
+              <button onClick={() => handleDoneTodo(index)}>完了</button>
+            )}
             <button onClick={() => handleDeleteTodo(index)}>削除</button>
           </li>
         ))}
@@ -48,5 +80,3 @@ function App() {
     </div>
   );
 };
-
-root.render(<App />);
